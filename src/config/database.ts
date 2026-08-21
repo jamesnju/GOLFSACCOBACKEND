dd47504b2694c4f4bf3b1b6805238966af49ea30
jamesnju <james500nju@gmail.com>
2026-08-21 09:48:30 +0300

    fix

--- src/config/database.ts
+++ src/config/database.ts
@@ -26,24 +26,3 @@ export const prisma = globalForPrisma.prisma ||
 if (env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
-
-// import { PrismaClient } from '@prisma/client';
-// import { PrismaPg } from '@prisma/adapter-pg';
-// import { Pool } from 'pg';
-// import { env } from './environment';
-
-// const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-// const connectionString = env.DATABASE_URL;
-// const pool = new Pool({ connectionString });
-// const adapter = new PrismaPg(pool);
-
-// export const prisma = globalForPrisma.prisma || 
-//   new PrismaClient({
-//     adapter,
-//     log: env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-//   });
-
-// if (env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-// export default prisma;
